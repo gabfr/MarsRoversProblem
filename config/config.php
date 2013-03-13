f2b1d6a9034021edc6a0cce4ee9840d9d93db716
gabfr <eu@gabrielf.com>
2013-03-13 08:18:25 -0300

    We don't use PHPUNIT_PATH anymore.

--- config/config.php
+++ config/config.php
@@ -2,6 +2,6 @@
 define("COMPLETE_PATH", "/Users/Gabriel/Sites/MarsRoversProblem");
 define("PHPUNIT_PATH", COMPLETE_PATH . "/vendor");
 
-set_include_path(get_include_path() . PATH_SEPARATOR . COMPLETE_PATH . PATH_SEPARATOR . PHPUNIT_PATH);
+set_include_path(get_include_path() . PATH_SEPARATOR . COMPLETE_PATH);
 
 require_once(COMPLETE_PATH . "/src/autoloader.php");
