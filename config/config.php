b602a997e7ae8ff37e27da1f75ff252af8acd830
gabfr <eu@gabrielf.com>
2013-03-13 09:02:49 -0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
-define("COMPLETE_PATH", dirname(__FILE__) . "..");
+define("COMPLETE_PATH", dirname(__FILE__) . "/..");
 define("PHPUNIT_PATH", COMPLETE_PATH . "/vendor");
 
 set_include_path(get_include_path() . PATH_SEPARATOR . COMPLETE_PATH);
